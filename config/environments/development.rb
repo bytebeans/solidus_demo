8d23345edebfd001ae8a04b23ab21a88c90f757c
chrislais <65403988+chrislais@users.noreply.github.com>
2022-02-28 16:10:54 +0800

    Configure action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -7,6 +7,16 @@ Rails.application.configure do
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
+  config.action_mailer.default_url_options = { host: 'localhost' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address: 'smtp.mxhichina.com',
+  port: 465,
+  user_name: 'hello@herlyst.com',
+  password: 'Enjoy5217',
+  ssl: true,
+  enable_starttls_auto: true,
+  authentication: :login }
 
   # Do not eager load code on boot.
   config.eager_load = false
